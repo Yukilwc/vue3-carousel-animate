b93932334a19dc942cc430b352865efb30a1ba67
Misaka <misaka1033@gmail.com>
2022-06-13 00:23:11 +0800

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import {resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry:resolve(__dirname, "components/main.ts"),
+      name: "vue3-carousel-animate",
+      fileName: (format) => `vue3-carousel-animate.${format}.ts`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
