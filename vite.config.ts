4960624789297ba2cf85bc6ae774a14da41735b6
Misaka <misaka1033@gmail.com>
2022-06-13 14:04:59 +0800

    u

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
-import typescript2 from "rollup-plugin-typescript2";
+// import typescript2 from "rollup-plugin-typescript2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
