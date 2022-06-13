430cc767ff1b6be0edb65b5152187fa09fb05b03
Misaka <misaka1033@gmail.com>
2022-06-13 13:21:05 +0800

    u

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
+import typescript2 from "rollup-plugin-typescript2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), typescript2()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/components/main.ts"),
